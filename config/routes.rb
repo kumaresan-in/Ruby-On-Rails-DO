8e23f83c3395a54a868546d22b677e475b9b0fd2
Kumaresan <kumaresh876@gmail.com>
2017-06-05 18:51:23 +0530

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :articles
+  resources :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
