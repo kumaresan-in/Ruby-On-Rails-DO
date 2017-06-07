a6e09ad22fd845e1936d8491742cd09a621a69a1
Kumaresan <kumaresh876@gmail.com>
2017-06-07 15:43:47 +0530

    User Edit And Forget Password Module Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
+ 
   root 'pages#home'
 
   get 'about' =>'pages#about'
 
-  resources :articles
-  resources :users
+  #resources :articles
+  #resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
